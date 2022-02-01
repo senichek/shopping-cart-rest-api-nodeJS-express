f1ebc0dde58437a6207468d282a261387924f504
Olexiy <senichek@gmail.com>
2022-02-01 12:09:10 +0100

    Populate DB when application starts

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 // Import the package
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
+const Item = require("./models/Item"); // Mongoose Schema
+const products = require('./productsCollection.json')
 require("dotenv/config");
 
 // Execute the package
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("REST API is running.");
-});
-
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 
 // Connect to DB, see .env file;
 mongoose.connect(
@@ -20,6 +23,11 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {}
 );
+
+// Remove all entries from DB on the application's start up.
+Item.deleteMany({}, () => {})
+// Populate DB on the application's start up.
+Item.insertMany(products);
 
 // DB Connection status - 0: disconnected; 1: connected; 2: connecting; 3: disconnecting;
 console.log("DB connection status: " + mongoose.connection.readyState);
