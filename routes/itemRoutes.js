80d2b5828be3226a8386300fc050c41ad8d38b48
Olexiy <senichek@gmail.com>
2022-02-09 21:41:14 +0100

    Bug fixing

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -19,6 +19,7 @@ router.get("/all", async (req, res) => {
   try {
     const items = await Item.find();
     res.json(items);
+    logger.info("getAll was called");
   } catch (err) {
     res.json({ message: err });
   }
@@ -93,6 +94,7 @@ router.patch("/:itemID", async (req, res) => {
             }
         }, { runValidators: true }
     );
+    logger.info("Udpated: " + req.body.title)
     res.json(updatedItem);
     }
   } catch (err) {
@@ -104,28 +106,7 @@ router.delete("/:itemID", async (req, res) => {
   try {
     const deleted = await Item.deleteOne({ _id: req.params.itemID });
     res.json(deleted);
-    logger.info("Deleted: " + deleted);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
-
-router.patch("/:itemID", async (req, res) => {
-  try {
-    const updatedItem = await Item.updateOne(
-      { _id: req.params.userID }, // _id - this is how ID looks in DB;
-      {
-        $set: {
-          title: req.body.title,
-          description: req.body.description,
-          price: req.body.price,
-          quantity: req.body.quantity,
-        },
-      },
-      { runValidators: true } // runValidators activates the validation of constraints from models.Item.js (Mongoose schema).
-    );
-    logger.info("Updated: " + updatedItem);
-    res.json(updatedItem);
+    logger.info("Deleted item with ID: " + req.params.itemID);
   } catch (err) {
     res.json({ message: err });
   }
@@ -135,23 +116,23 @@ router.post("/updateQuantity", async (req, res) => {
   try {
     let updatedItems = [];
     for (let i = 0; i < req.body.length; i++) {
-      // Delete items with old quantity and recreate them with new quantity;
-      const itemToSave = new Item({
-        title: req.body[i].title,
-        description: req.body[i].description,
-        price: req.body[i].price,
-        quantity: req.body[i].quantity,
-      });
-
-      await Item.deleteOne({ _id: req.body[i]._id});
-      /* The <.save()> method is called on the item/object and not on Schema,
-      this is why we re-created the <itemToSave> above to access <save()> method. */
-      let updated = await itemToSave.save();
-      updatedItems.push(updated);
-    }
+      await Item.updateOne(
+        { _id: req.body[i]._id },  // _id - this is how ID looks in DB;
+        {
+            $set:
+            {
+                title: req.body[i].title,
+                description: req.body[i].description,
+                price: req.body[i].price,
+                quantity: req.body[i].quantity
+            }
+        }, { runValidators: true }
+    );
+    updatedItems.push(req.body[i].title);
+  }
     
-    logger.info("Updated quantity: " + updatedItems);
-    res.json(updated);
+    logger.info("Updated quantity of " + updatedItems.map(title => title));
+    res.json(updatedItems);
 
   } catch (err) {
     res.json({ message: err });
