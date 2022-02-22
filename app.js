f782bab19f35a5d3c096b35527de71c08f3c81f3
Olexiy <senichek@gmail.com>
2022-02-22 16:42:54 +0100

    Changed to In-memory MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Import the package
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const Item = require("./models/Item"); // Mongoose Schema
 const User = require("./models/User"); // Mongoose Schema
-const products = require('./productsCollection.json')
-const users = require('./usersCollection.json')
+const products = require("./productsCollection.json");
+const users = require("./usersCollection.json");
 require("dotenv/config");
-const swaggerConfig = require("./swagger/config.js")
+const swaggerConfig = require("./swagger/config.js");
+const mongoDB = require("./tests/configs/mongoDB.js"); // In-memory mongoDB
 
 /* swaggerJsdoc = require("swagger-jsdoc"),
 swaggerUi = require("swagger-ui-express"); */
@@ -19,30 +20,37 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.use(cors({
+app.use(
+  cors({
     //origin: ['http://localhost:3000']
-    origin: ['https://shopping-cart-front-react.herokuapp.com']
-}));
+    origin: ["https://shopping-cart-front-react.herokuapp.com"],
+  })
+);
 
 // Connect to DB, see .env file;
-mongoose.connect(
+/* mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {}
-);
+); */
+
+// Connecting to in-memory mongoDB
+mongoDB.connectDB;
 
 // DB Connection status - 0: disconnected; 1: connected; 2: connecting; 3: disconnecting;
-console.log("DB connection status: " + mongoose.connection.readyState);
+//console.log("DB connection status: " + mongoose.connection.readyState);
 
 // Repopulate DB on the application's start up.
 // We use async to wait till db is ready.
- const populateDB = () => {
- Item.deleteMany({}, () => {})
- Item.insertMany(products);
- User.deleteMany({}, () => {})
- User.insertMany(users);
-  console.log("================DB has been populated===============.")
-}
+const populateDB = () => {
+  User.deleteMany({}, () => {});
+  User.insertMany(users);
+
+  Item.deleteMany({}, () => {});
+  Item.insertMany(products);
+
+  console.log("================DB has been populated===============.");
+};
 populateDB();
 
 // Import routes;
@@ -54,7 +62,11 @@ app.use("/item", itemRoute);
 app.use("/user", userRoute);
 
 // Swagger. See swagger/config.js
-app.use(swaggerConfig.docsURL, swaggerConfig.swaggerServe, swaggerConfig.swaggerSetup);
+app.use(
+  swaggerConfig.docsURL,
+  swaggerConfig.swaggerServe,
+  swaggerConfig.swaggerSetup
+);
 
 app.listen(process.env.PORT);
 //app.listen();
